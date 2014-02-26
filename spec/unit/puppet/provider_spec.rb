aeb2b35b1de037a8afe1dac7167b864efbe7cbd0
Tomas Doran <tdoran@yelp.com>
2014-02-26 16:48:33 +0000

    Re-arrange fixtures directory to be in a more sane place

--- spec/unit/puppet/provider_spec.rb
+++ spec/unit/puppet/provider_spec.rb
@@ -20,7 +20,7 @@ describe provider_class do
   end
 
   def load_fix(type, name)
-    provider.expects(:ethtool).with("-#{type}", 'eth0').returns(IO.read("#{File.dirname(__FILE__)}/../../../fixtures/ethtool_outputs/#{type}/#{name}.txt"))
+    provider.expects(:ethtool).with("-#{type}", 'eth0').returns(IO.read("#{File.dirname(__FILE__)}/../../fixtures/ethtool_outputs/#{type}/#{name}.txt"))
   end
 
   ['centos5_1', 'ubuntulucid_1'].each do |fixture_name|
