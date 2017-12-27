2161d2811511764df769fa1302cd09fe4cc194f2
Jason Pereira <mindriot88@users.noreply.github.com>
2017-12-27 15:09:00 +0000

    move loop break to a single line

--- lib/facter/interface_driver.rb
+++ lib/facter/interface_driver.rb
@@ -2,8 +2,7 @@ require 'facter'
 require 'facter/util/ip'
 require 'json'
 Facter::Util::IP.get_interfaces.each do |interface|
-  next if interface.start_with?('veth')
-  next if ! File.exists?('/sbin/ethtool') 
+  next if interface.start_with?('veth') || ! File.exists?('/sbin/ethtool') 
   Facter.debug("Running ethtool on interface #{interface}")
   data = {}
   Facter::Util::Resolution.exec("ethtool -i #{interface} 2>/dev/null").split("\n").each do |line|
